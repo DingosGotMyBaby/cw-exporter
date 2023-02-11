cbfa6cddc200a31b8c51b756c1780f85cf8c87b9
Dingo <7553117+DingosGotMyBaby@users.noreply.github.com>
2023-02-11 14:05:02 +1100

    fix(app): add help text for release

--- CW-Exporter/Program.cs
+++ CW-Exporter/Program.cs
@@ -12,7 +12,11 @@ namespace CWExporter
             // check that arguments were supplied
             if (args.Length == 0)
             {
-                Console.WriteLine("No arguments supplied. Please supply a file by dragging a compatible file onto the exe.");
+                Console.WriteLine("No files supplied. Please supply a file by dragging a compatible file onto the exe.");
+                Console.WriteLine("Valid File Types are:");
+                Console.WriteLine("ydd, ytd, yft, ydr, ybn");
+                Console.WriteLine("Please ensure you are using one of the above file types or an XML of the same intended type.");
+                
                 // press any button to continue prompt
                 Console.WriteLine("Press any key to continue...");
                 Console.ReadKey();
@@ -63,11 +67,11 @@ namespace CWExporter
         
         private enum FileTypes
         {
-            Ydd,
-            Ytd,
-            Yft,
-            Ydr,
-            Ybn
+            ydd,
+            ytd,
+            yft,
+            ydr,
+            ybn
         }
         
         private static byte[] ConvertFromXml(string fileName, FileTypes fileType)
@@ -79,22 +83,22 @@ namespace CWExporter
             // switch based on file type
             switch (fileType)
             {
-                case FileTypes.Ydd:
+                case FileTypes.ydd:
                     YddFile yddFile = XmlYdd.GetYdd(xmlDocument);
                     return yddFile.Save();
-                case FileTypes.Ytd:
+                case FileTypes.ytd:
                     YtdFile ytdFile = XmlYtd.GetYtd(xmlDocument);
                     return ytdFile.Save();
 
-                case FileTypes.Yft:
+                case FileTypes.yft:
                     YftFile yftFile = XmlYft.GetYft(xmlDocument);
                     return yftFile.Save();
 
-                case FileTypes.Ydr:
+                case FileTypes.ydr:
                     YdrFile ydrFile = XmlYdr.GetYdr(xmlDocument);
                     return ydrFile.Save();
 
-                case FileTypes.Ybn:
+                case FileTypes.ybn:
                     YbnFile ybnFile = XmlYbn.GetYbn(xmlDocument);
                     return ybnFile.Save();
 
@@ -112,35 +116,35 @@ namespace CWExporter
             // switch based on file type to convert to xml
             switch (fileType)  
             {
-                case FileTypes.Ydd:
+                case FileTypes.ydd:
                     XmlDocument yddXml = new XmlDocument();
                     YddFile yddFile = new YddFile();
                     yddFile.Load(rawFileBytes);
                     yddXml.LoadXml(YddXml.GetXml(yddFile));
                     yddXml.Save(outputFilePath);
                     break;
-                case FileTypes.Ybn:
+                case FileTypes.ybn:
                     XmlDocument ybnXml = new XmlDocument();
                     YbnFile ybnFile = new YbnFile();
                     ybnFile.Load(rawFileBytes);
                     ybnXml.LoadXml(YbnXml.GetXml(ybnFile));
                     ybnXml.Save(outputFilePath);
                     break;
-                case FileTypes.Ydr:
+                case FileTypes.ydr:
                     XmlDocument ydrXml = new XmlDocument();
                     YdrFile ydrFile = new YdrFile();
                     ydrFile.Load(rawFileBytes);
                     ydrXml.LoadXml(YdrXml.GetXml(ydrFile));
                     ydrXml.Save(outputFilePath);
                     break;
-                case FileTypes.Yft:
+                case FileTypes.yft:
                     XmlDocument yftXml = new XmlDocument();
                     YftFile yftFile = new YftFile();
                     yftFile.Load(rawFileBytes);
                     yftXml.LoadXml(YftXml.GetXml(yftFile));
                     yftXml.Save(outputFilePath);
                     break;
-                case FileTypes.Ytd:
+                case FileTypes.ytd:
                     XmlDocument ytdXml = new XmlDocument();
                     YtdFile ytdFile = new YtdFile();
                     ytdFile.Load(rawFileBytes);
